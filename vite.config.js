63fa136ab6e87616ce2429e6e899a0e78e15220b
koushik murali <koushikaadhy@gmail.com>
2025-03-08 20:33:00 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,3 +1,6 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
 export default defineConfig({
   plugins: [react()],
   base: './', // Make sure this is correct for relative paths
@@ -9,4 +12,4 @@ export default defineConfig({
       }
     }
   }
-});
+})
